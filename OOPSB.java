5d45860b24f7b171cad23bccd7b56453504d494a
himanshuydv9214 <h9214855@gmail.com>
2026-02-17 11:02:31 +0530

    Added OOPSB.java to uc2 branch

---
+++ OOPSB.java
@@ -0,0 +1,16 @@
+public class  OOPSB{
+
+    public static void main(String[] args) {
+        System.out.println("   **   " + "   " + "   **   " + "   " + " **********   " + "   *********");
+        System.out.println(" **  ** " + "   " + " **  ** " + "   " + " **      **   " + " **         ");
+        System.out.println("*      *" + "   " + "*      *" + "   " + " **      **   " + "*           ");
+        System.out.println("*      *" + "   " + "*      *" + "   " + " **      **   " + "*           ");
+        System.out.println("*      *" + "   " + "*      *" + "   " + " **********   " + " ********   ");
+        System.out.println("*      *" + "   " + "*      *" + "   " + " **********   " + "         ** ");
+        System.out.println("*      *" + "   " + "*      *" + "   " + " **           " + "           *");
+        System.out.println("*      *" + "   " + "*      *" + "   " + " **           " + "           *");
+        System.out.println(" **  ** " + "   " + " **  ** " + "   " + " **           " + "         ** ");
+        System.out.println("   **   " + "   " + "   **   " + "   " + " **           " + "*********   ");
+    }
+
+}
