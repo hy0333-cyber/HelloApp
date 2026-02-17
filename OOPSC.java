6085ecb55af6ecd202a4d8e791f5912717662628
himanshuydv9214 <h9214855@gmail.com>
2026-02-17 11:19:35 +0530

    Added OOPSC.java to uc3 branch

---
+++ OOPSC.java
@@ -0,0 +1,15 @@
+public class OOPSC {
+    public static void main(String[] args) {
+
+        String output = String.join("\n",
+                " *****   *****   ****    ****  ",
+                "*     * *     *  *   *  *      ",
+                "*     * *     *  ****    ***    ",
+                "*     * *     *  *           *  ",
+                " *****   *****   *      ****    "
+        );
+
+        System.out.println(output);
+    }
+}
+
