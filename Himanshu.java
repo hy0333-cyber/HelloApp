fba28469fbe2dd1f6a989e98890bf4240706f3ff
himanshuydv9214 <h9214855@gmail.com>
2026-02-17 09:53:36 +0530

    Initial commit in UC1

---
+++ Himanshu.java
@@ -0,0 +1,12 @@
+public class Himanshu{
+    public static void main (String[] args){
+        int x;
+        int y;
+        int sum;
+        x=20;
+        y=40;
+        sum=x+y;
+        System.out.println("Sum: "+sum);
+    }
+    
+}
