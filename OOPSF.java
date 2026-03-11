954a22de21d02654a68e2f0e7df10382821bbf35
himanshuydv9214 <h9214855@gmail.com>
2026-03-11 08:32:32 +0530

    Added OOPSF.java to uc6 branch

---
+++ OOPSF.java
@@ -0,0 +1,68 @@
+public class OOPSF {
+    public static class CharacterPatternMap {
+        private final char character;
+        private final String[] pattern;
+
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    private static String buildLine(CharacterPatternMap[] chars, int lineIndex) {
+        StringBuilder line = new StringBuilder();
+        for (CharacterPatternMap cp : chars) {
+            line.append(cp.getPattern()[lineIndex]).append("  ");
+        }
+        return line.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] oPattern = {
+            " **** ",
+            "*    *",
+            "*    *",
+            "*    *",
+            "*    *",
+            "*    *",
+            " **** "
+        };
+        String[] pPattern = {
+            "***** ",
+            "*    *",
+            "*    *",
+            "***** ",
+            "*     ",
+            "*     ",
+            "*     "
+        };
+        String[] sPattern = {
+            " *****",
+            "*     ",
+            "*     ",
+            " *****",
+            "     *",
+            "     *",
+            " *****"
+        };
+
+        CharacterPatternMap[] oopsChars = {
+            new CharacterPatternMap('O', oPattern),
+            new CharacterPatternMap('O', oPattern),
+            new CharacterPatternMap('P', pPattern),
+            new CharacterPatternMap('S', sPattern)
+        };
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(buildLine(oopsChars, i));
+        }
+    }
+}
